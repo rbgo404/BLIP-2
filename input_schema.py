0469fa4a0e6a452ed5de73e544a7f613de961d1f
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-20 19:54:26 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -1,14 +1,14 @@
 INPUT_SCHEMA = {
-    "prompt": {
-        'datatype': 'STRING',
-        'required': True,
-        'shape': [1],
-        'example': ["Explain the image."]
-    },
-    "img_url": {
+        "img_url": {
         'datatype': 'STRING',
         'required': True,
         'shape': [1],
         'example': ["https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg"]
+    },
+    "prompt": {
+        'datatype': 'STRING',
+        'required': False,
+        'shape': [1],
+        'example': ["Explain the image."]
     }
 }
