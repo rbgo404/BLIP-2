4044cf054fb9b73e9bbdd78512b287aec53a3bae
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-09-20 19:38:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ class InferlessPythonModel:
         self.model = Blip2ForConditionalGeneration.from_pretrained(model_id, device_map="cuda")
 
     def infer(self, inputs):
-        prompt = inputs["prompt"]  # Extract the prompt from the input
         img_url = inputs["img_url"]
+        prompt = inputs.get("prompt")
 
         raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
-        inputs = self.processor(raw_image, prompt, return_tensors="pt").to("cuda")
+        inputs = self.processor(raw_image, prompt,return_tensors="pt").to("cuda")
 
         out = self.model.generate(**inputs)
         return {"generated_result": self.processor.decode(out[0], skip_special_tokens=True).strip()}
